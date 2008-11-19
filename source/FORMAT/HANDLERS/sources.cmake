8f55eea6e241801ae6fb2bd718b628da9d22fdf7
Chris Bielow <chris.bielow@fu-berlin.de>
2008-11-19 13:56:32 +0000

    added conditional ANDI_MS compile
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/Retreat2008@4073 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/FORMAT/HANDLERS/sources.cmake
@@ -0,0 +1,34 @@
+### the directory name
+set(directory source/FORMAT/HANDLERS)
+
+
+### list all filenames of the directory here
+set(sources_list
+ ConsensusXMLHandler.C
+ FeatureXMLHandler.C
+ MascotXMLHandler.C
+ MzDataHandler.C
+ MzMLHandler.C
+ MzXMLHandler.C
+ ParamXMLHandler.C
+ PTMXMLHandler.C
+ UnimodXMLHandler.C
+ XMLHandler.C
+ XTandemInfileXMLHandler.C
+)
+
+
+if (USE_ANDIMS)
+	list(APPEND sources ANDIHandler.C)
+endif()
+
+### add path to the filenames
+set(sources)
+foreach(i ${sources_list})
+  list(APPEND sources ${directory}/${i})
+  message(${i})
+endforeach(i)
+
+### pass source file list to the upper instance
+set(OpenMS_sources ${OpenMS_sources} ${sources})
+
---
+++ source/FORMAT/sources.cmake
@@ -0,0 +1,69 @@
+### the directory name
+set(directory source/FORMAT)
+
+
+### list all filenames of the directory here
+set(sources_list
+Base64.C
+ConsensusXMLFile.C
+ControlledVocabulary.C
+CVMappingFile.C
+CVMappings.C
+DTA2DFile.C
+DTAFile.C
+FASTAFile.C
+FastaIterator.C
+FastaIteratorIntern.C
+FeatureXMLFile.C
+FileHandler.C
+IdXMLFile.C
+InspectInfile.C
+InspectOutfile.C
+LibSVMEncoder.C
+MascotInfile.C
+MascotInfile2.C
+MascotOutfile.C
+MascotRemoteQuery.C
+MascotXMLFile.C
+MS2File.C
+MSPFile.C
+MzDataFile.C
+MzMLFile.C
+MzXMLFile.C
+OMSSACSVFile.C
+OMSSAXMLFile.C
+PeakFileOptions.C
+PeakTypeEstimator.C
+PepNovoInfile.C
+PepNovoOutfile.C
+PepXMLFile.C
+PersistentObject.C
+PTMXMLFile.C
+SequestInfile.C
+SequestOutfile.C
+TextFile.C
+TransformationXMLFile.C
+UnimodXMLFile.C
+UniqueIdGenerator.C
+XMLFile.C
+XMLValidator.C
+XTandemInfile.C
+XTandemXMLFile.C
+)
+
+
+if (USE_ANDIMS)
+	list(APPEND sources ANDIFile.C)
+endif()
+
+### add path to the filenames
+set(sources)
+foreach(i ${sources_list})
+  list(APPEND sources ${directory}/${i})
+  message(${i})
+endforeach(i)
+
+### pass source file list to the upper instance
+set(OpenMS_sources ${OpenMS_sources} ${sources})
+
+include(source/FORMAT/HANDLERS/sources.cmake)

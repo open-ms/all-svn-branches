eeaefa7112a8661f0c34019979f8174b328514de
Stephan Aiche <stephan.aiche@fu-berlin.de>
2010-06-24 16:23:45 +0000

    fixed syntax error in sources.cmake
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/sass@7073 6adb6e08-d915-0410-941f-83917bcadc18

--- source/FILTERING/DATAREDUCTION/sources.cmake
+++ source/FILTERING/DATAREDUCTION/sources.cmake
@@ -4,8 +4,8 @@ set(directory source/FILTERING/DATAREDUCTION)
 ### list all filenames of the directory here
 set(sources_list
 DataFilters.C
-SILACFilter
-SILACFiltering
+SILACFilter.C
+SILACFiltering.C
 )
 
 ### add path to the filenames

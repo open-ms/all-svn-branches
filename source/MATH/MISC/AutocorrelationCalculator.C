8e9a08622d94588b9f89e21373039e4f86989890
Steffen Sass <steffensass@users.sourceforge.net>
2010-08-17 10:11:21 +0000

    Autocorrelation calculator update: Fourier transformation included
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/sass@7232 6adb6e08-d915-0410-941f-83917bcadc18

--- source/MATH/MISC/AutocorrelationCalculator.C
+++ source/MATH/MISC/AutocorrelationCalculator.C
@@ -32,7 +32,9 @@
 #include <gsl/gsl_spline.h>
  #include <gsl/gsl_integration.h>
 #include <gsl/gsl_errno.h>
-
+#include <gsl/gsl_fft_complex.h>
+#include <gsl/gsl_fft_halfcomplex.h>
+#include <cmath>
 
 
 namespace OpenMS
@@ -78,27 +80,9 @@ void AutocorrelationCalculator::calculate(const MSExperiment<Peak1D>& input, MSE
 	endProgress();
 }
 
-struct f_params{ gsl_spline* interpolation; gsl_interp_accel* acc_spl; DoubleReal shift; DoubleReal max_intensity; DoubleReal max_mz;};
-
-DoubleReal f (DoubleReal x, void * params)
-{
-	struct f_params* pList = (struct f_params *) params;
-	gsl_spline* interpolation = pList->interpolation;
-	gsl_interp_accel* acc_spl = pList->acc_spl;
-	DoubleReal shift = pList->shift;
-	DoubleReal max_intensity = pList->max_intensity;
-	DoubleReal max_mz = pList->max_mz;
-	DoubleReal f_x=gsl_spline_eval (interpolation, x, acc_spl);
-	DoubleReal f_x_shifted=gsl_spline_eval (interpolation, x+shift, acc_spl);
-	DoubleReal f=0.0;
-	if (x+shift <= max_mz)
-		f =  f_x*f_x_shifted;
-	return (f/max_intensity);
-}
-
-
 void AutocorrelationCalculator::analyzeSpectrum(const MSSpectrum<Peak1D>& input, MSSpectrum<Peak1D>& output)
 {
+	//Copy spectrum settings
 	output.clear(true);
 	output.SpectrumSettings::operator=(input);
 	output.MetaInfoInterface::operator=(input);
@@ -109,46 +93,61 @@ void AutocorrelationCalculator::analyzeSpectrum(const MSSpectrum<Peak1D>& input,
 
 	std::vector<DoubleReal> mz_vec;
 	std::vector<DoubleReal> intensity_vec;
-//	DoubleReal last_mz=input.begin()->getMZ();
 	mz_min=input.begin()->getMZ();
+
+	//Fill intensity and m/z vector for interpolation
 	for (MSSpectrum<Peak1D>::ConstIterator mz_it=input.begin(); mz_it!=input.end(); ++mz_it)
 	{
 		mz_vec.push_back(mz_it->getMZ());
 		intensity_vec.push_back(mz_it->getIntensity());
 	}
 
+	//Interpolate
 	gsl_interp_accel* acc_spl = gsl_interp_accel_alloc();
 	gsl_spline* spline_spl = gsl_spline_alloc(gsl_interp_akima, mz_vec.size());
 	gsl_spline_init(spline_spl, &(*mz_vec.begin()), &(*intensity_vec.begin()), mz_vec.size());
 
-
-	if (input.size() < 6) return;
-
 	DoubleReal max_distance=mz_max-mz_min;
 
-	for (DoubleReal shift=0.0;shift<=max_distance;shift+=stepwidth_)
+	//n must be a power of two for gsl Fourier transformation; take next higher size for n, which is a power of two
+	Size s = pow(2,(ceil(log2(max_distance/stepwidth_))));
+
+	//Create a vector containing interpolated values with a spacing of "stepwidth"
+	std::vector<DoubleReal> data(s,0);
+	Size i=0;
+	for (DoubleReal x=mz_min;x<=mz_max;x+=stepwidth_)
 	{
-		std::vector<DoubleReal> multiplicated_vec;
-		for (MSSpectrum<Peak1D>::ConstIterator mz_it=input.begin(); mz_it!=input.end(); ++mz_it)
-		{
-			multiplicated_vec.push_back(mz_it->getIntensity()*gsl_spline_eval (spline_spl, mz_it->getMZ()+shift, acc_spl));
-		}
+		data[i] = gsl_spline_eval (spline_spl, x, acc_spl);
+		++i;
+	}
 
-		gsl_interp_accel* acc_spl_mult = gsl_interp_accel_alloc();
-		gsl_spline* spline_spl_mult = gsl_spline_alloc(gsl_interp_akima, mz_vec.size());
-		gsl_spline_init(spline_spl_mult, &(*mz_vec.begin()), &(*multiplicated_vec.begin()), mz_vec.size());
+	//Fast fourier transform the values
+	gsl_fft_real_radix2_transform (&(*data.begin()), 1, s);
 
-		DoubleReal result=gsl_spline_eval_integ (spline_spl_mult,mz_min,mz_max, acc_spl_mult);
+	//Multiply the fast fourier transformed complex values with the complex conjugate
+	//Have a look at the GNU Scientific Library reference manual for a description of the data structure
+	data[0]=data[0]*data[0];
+	data[s/2]=data[s/2]*data[s/2];
+	for (i = 1; i <= s/2; ++i)
+	{
+		data[i]=(data[i]*data[i]+data[s-i]*data[s-i]);
+		data[s-i]=0.0;
+	}
+
+	//Compute inverse fast fourier transformation
+	gsl_fft_halfcomplex_radix2_inverse (&(*data.begin()), 1, s);
+
+	//Create output
+	DoubleReal shift=0.0;
+	for (i = 0; i <= s/2 ; ++i)
+	{
 		Peak1D peak;
 		peak.setMZ(shift);
-		peak.setIntensity(result);
-		if (result > 0)
+		peak.setIntensity(data[i]);
+		if (data[i] > 0)
 			output.push_back(peak);
-		gsl_spline_free(spline_spl_mult);
-		gsl_interp_accel_free(acc_spl_mult);
+		shift+=stepwidth_;
 	}
-	gsl_spline_free(spline_spl);
-	gsl_interp_accel_free(acc_spl);
 }
 
 }

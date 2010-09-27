eb11753814b6b7023b380facb99d15056e40382f
Hendrik Brauer <hbrauer@users.sourceforge.net>
2010-09-27 10:19:13 +0000

    my first TOPPTOOL "HelloWorld" Hendrik
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/brauer@7420 6adb6e08-d915-0410-941f-83917bcadc18

---
+++ source/APPLICATIONS/TOPP/HelloWorld.C
@@ -0,0 +1,102 @@
+// -*- mode: C++; tab-width: 2; -*-
+// vi: set ts=2:
+//
+// --------------------------------------------------------------------------
+//                   OpenMS Mass Spectrometry Framework
+// --------------------------------------------------------------------------
+//  Copyright (C) 2003-2010 -- Oliver Kohlbacher, Knut Reinert
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Clemens Groepl $
+// $Authors: Marc Sturm, Clemens Groepl, Steffen Sass $
+// --------------------------------------------------------------------------
+
+#include <OpenMS/FORMAT/FileHandler.h>
+#include <OpenMS/FORMAT/FileTypes.h>
+
+
+#include <OpenMS/APPLICATIONS/TOPPBase.h>
+
+using namespace OpenMS;
+using namespace std;
+
+//-------------------------------------------------------------
+//Doxygen docu
+//-------------------------------------------------------------
+
+/**
+	@page TOPP_HelloWorld HelloWorld
+
+	@brief First trial of my own class.
+
+BlaBla
+*/
+
+// We do not want this class to show up in the docu:
+/// @cond TOPPCLASSES
+
+class TOPPHelloWorld
+  : public TOPPBase
+{
+
+public:
+	TOPPHelloWorld()
+		: TOPPBase("HelloWorld","First trial of my own class")
+	{
+	}
+
+protected:
+	void registerOptionsAndFlags_()
+	{
+		registerInputFile_("in","<file>","","input file ");
+		setValidFormats_("in",StringList::create("mzML"));
+		registerOutputFile_("out","<file>","","Output file",true);
+		setValidFormats_("out",StringList::create("mzML"));
+		registerStringOption_("string","<name>","","String input",true);
+	}
+
+
+	ExitCodes main_(int , const char**)
+	{
+		//-------------------------------------------------------------
+		// parameter handling
+		//-------------------------------------------------------------
+		String in = getStringOption_("in");
+
+		MzMLFile f;
+		MSExperiment<Peak1D> exp;
+		f.load(in,exp);
+
+		String out = getStringOption_("out");
+		f.store(out,exp);
+
+		String str = getStringOption_("string");
+
+		cout << str << endl;
+
+		return EXECUTION_OK;
+	}
+};
+
+
+int main( int argc, const char** argv )
+{
+  TOPPHelloWorld tool;
+  return tool.main(argc,argv);
+}
+
+/// @endcond

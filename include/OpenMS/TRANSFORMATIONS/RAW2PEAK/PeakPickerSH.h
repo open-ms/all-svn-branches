e90f08f2df0aa6c1cf4910432075a0e2b130a647
Florian Zeller <fzeller@users.sourceforge.net>
2010-10-15 17:04:24 +0000

    cosmetic changes (soft-tabs)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/superhirn@7478 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/TRANSFORMATIONS/RAW2PEAK/PeakPickerSH.h
+++ include/OpenMS/TRANSFORMATIONS/RAW2PEAK/PeakPickerSH.h
@@ -11,27 +11,26 @@
 
 namespace OpenMS
 {
-
   class OPENMS_DLLAPI PeakPickerSH
-		: public DefaultParamHandler,
-			public ProgressLogger
+    : public DefaultParamHandler,
+      public ProgressLogger
   {
-	public:
+  public:
     PeakPickerSH();
 		
     virtual ~PeakPickerSH();
 		
     /** 
-		 @brief Picks one peak.
-		 */
+     @brief Picks one peak.
+    */
     template <typename PeakType>
     void pick(const MSSpectrum<PeakType>& input, MSSpectrum<PeakType>& output, float fWindowWidth);
 		
     /** 
-				@brief Applies the peak-picking algorithm to a map (MSExperiment). This method picks peaks for each scan in the map consecutively. The resulting picked peaks are written to the output map.
+     @brief Applies the peak-picking algorithm to a map (MSExperiment). This method picks peaks for each scan in the map consecutively. The resulting picked peaks are written to the output map.
     */
     void pickExperiment(const MSExperiment<>& input, MSExperiment<>& output);
-	};	
+  };	
 }
 
 #endif

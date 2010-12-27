dd7fdcc8ba864160be1c07fc929c0ddf00e62573
Florian Zeller <fzeller@users.sourceforge.net>
2010-12-27 12:24:03 +0000

    Difference to 1928: Changed signature: - void set_current_indexes(); -> void set_current_indexes(double pminrt, double pmaxrt); - void read_mzXML_DATA(); -> void read_mzXML_DATA(Vec datavec); - void get_MS_scan(off_t); -> void get_MS_scan(off_t IN, double TR, RawData* data); - void processMS1InputData(int , float, MY_RAMP_PEAKS* ); -> void processMS1InputData(int , float, RawData* data); Commented: - typedef RAMPFILE RAMP_FILE; - MS2_Process_Data* MS2_LC_MS_DATA_PROCESSOR; - RAMP_FILE* ramp_file_Struct; - open_mzxml_file(); - close_mzxml_file(); - processMS2InputData(int, double, float, int, MY_RAMP_PEAKS*); - int setInterMonoIsotopicLCDistance( int , int, int, double); - int getPreviousMSXScan( int, int ); - findMS1PrecursorData( double* , int , int, int); - print_scan_header( ScanHeaderStruct* ); - RAMP_FILE* get_Ramp_file_handler(){return ramp_file_Struct;}; - string get_current_file(){return current_file;}; - set_current_file(string IN){current_file = IN;}; - MS2_Process_Data* get_processed_MS2_data_structure(){return MS2_LC_MS_DATA_PROCESSOR;}; - convert_ms_peaks(int, double, list<DeconvPeak>&,vector<ms_peak>&); - delete_existing_debug_file();
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/superhirn@7721 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/SUPERHIRN/FT_PEAK_DETEC_mzXML_reader.h
+++ include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/SUPERHIRN/FT_PEAK_DETEC_mzXML_reader.h
@@ -35,6 +35,8 @@
 // - for old ramp use FILE*
 // - for new cramp use RAMPFILE*
 typedef FILE* MZXML_FILE;
+//typedef RAMPFILE RAMP_FILE;
+
 
 
 class FT_PEAK_DETEC_mzXML_reader{
@@ -46,9 +48,11 @@ class FT_PEAK_DETEC_mzXML_reader{
 private:
   
   Process_Data* MS1_LC_MS_DATA_PROCESSOR;
+  // MS2_Process_Data* MS2_LC_MS_DATA_PROCESSOR;
 
   string current_file;
   //MZXML_FILE file_handler;
+  //RAMP_FILE* ramp_file_Struct;
   
   off_t index_offset;
   off_t* scan_index;
@@ -93,14 +97,23 @@ public:
   ~FT_PEAK_DETEC_mzXML_reader();
   // class constructor
   FT_PEAK_DETEC_mzXML_reader();
+  
+  //////////////////////////////////////////////////
+  // file open / read / close function
+  // set the current mzXML file;
+  //void open_mzxml_file();
+  // close the current mzXML file;
+  //void close_mzxml_file();
   // set indexes of the current mzXML file;
   void set_current_indexes(double pminrt, double pmaxrt);
-      
+  
+  // reads the ms data from a mzXML file opened by teh handler
+  void read_mzXML_DATA(Vec datavec);
+
   // get a MS scan at a given scan number within
   // a mass range and stores it into the ms region structure
   void get_MS_scan(off_t IN, double TR, RawData* data);
-  
-  void read_mzXML_DATA(Vec datavec);
+
   
   ///////////////////////////////////////////////////////
   // MS1 level processing routine functions:
@@ -110,16 +123,53 @@ public:
   // - add them to Process_Data Structure:
   void processMS1InputData(int , float, RawData* data);
  
+  
+  ///////////////////////////////////////////////////////
+  // MS2 level processing routine functions:
+  // process the MS2 level input data:
+  // - construct a RawData object with input peaks
+  // - centoid them
+  // - add them to Process_Data Structure:
+  //void processMS2InputData(int , double, float, int,  MY_RAMP_PEAKS* );
+  
+  
+
   // set the maximal inter-monoisotopic distance
   // for the same LC-elution peak
   int setInterMonoIsotopicLCDistance( int , int, int);
+  //int setInterMonoIsotopicLCDistance( int , int, int, double);
   
+  // get a previous scan at the MS level from the current scan
+  //int getPreviousMSXScan( int, int );
+
+  
+  ///////////////////////////////////////////////////////////////////////////////
+  // go back to the MS1 level and
+  // find the correct precursor mass by mz and z:
+  //void findMS1PrecursorData( double* , int , int, int);
+    
+
   ///////////////////////////////////////////////////////////////////////////////
   // check if the scan number should be processed by MS Precursor Mass extraction
   bool checkMSPrecursorMassScan( int );
   // check if the scan number should be processed as MSn FragmentMass spectrum
   bool checkMSFragmentMassScan( int );
+
   
+  // print the scan header:
+  //void print_scan_header( ScanHeaderStruct* );  
+  
+  ///////////////////////////////
+  // start here all the get / set
+  // function to access the
+  // variables of the class
+  
+  // set the current mzXML file handler;
+  //void set_file_handler(MZXML_FILE IN){file_handler = IN;};
+  //MZXML_FILE get_file_handler(){return file_handler;};
+  //RAMP_FILE* get_Ramp_file_handler(){return ramp_file_Struct;};
+  //MZXML_FILE get_Ramp_file_handler(){return file_handler;};
+
   // total scan access:
   int get_total_scan(){return total_scan;};
   off_t get_index_off_set(){return index_offset;};
@@ -127,12 +177,24 @@ public:
   // get a scan with the index:
   off_t get_scan(int i){return scan_index[i];};
   
+  
+  //string get_current_file(){return current_file;};
+  //void set_current_file(string IN){current_file = IN;};
+
   // get the MS1 processed data:
   Process_Data* get_processed_MS1_data_structure(){return MS1_LC_MS_DATA_PROCESSOR;};
+  // get the MS2 processed data:
+  //MS2_Process_Data* get_processed_MS2_data_structure(){return MS2_LC_MS_DATA_PROCESSOR;};
 
   // build up an index scan vs retention time:
   void insert_into_scan_TR_index(int IN, float TR){Process_Data::insert_into_scan_TR_index(IN, TR);};
   
+  // converts DeconvPeak list to ms_peak vector
+  //void convert_ms_peaks(int, double, list<DeconvPeak>&,vector<ms_peak>&);
+
+  // delete existing debug files before writing in append mode
+  //void delete_existing_debug_file();
+
 };
 
 #endif

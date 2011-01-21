e2e3800d8922f5f8eec7e1cd1d6a315f4ae66789
Florian Zeller <fzeller@users.sourceforge.net>
2011-01-21 09:26:20 +0000

    Added fix to use native scan id for re-ordering (necessary to achieve same results in TOPPView as in TOPPAS)
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/superhirn@7805 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderAlgorithmSH.h
+++ include/OpenMS/TRANSFORMATIONS/FEATUREFINDER/FeatureFinderAlgorithmSH.h
@@ -26,6 +26,24 @@ namespace OpenMS
       this->check_defaults_ =  false;
     }
     
+    static unsigned int getNativeScanId(String native_id)
+    {
+      Size num_pos=0;
+      while(!isdigit(native_id[num_pos]) && num_pos<native_id.length())
+      {
+        ++num_pos;
+      }
+      if(num_pos==native_id.length())
+      {
+        std::cout << "Native id could not be determined: " << native_id;
+        throw Exception::InvalidParameter(__FILE__,__LINE__,__PRETTY_FUNCTION__, "Cannot convert native id to unsigned integer");
+      }
+      else
+      {
+        return native_id.substr(num_pos).toInt();
+      }
+    }
+    
     virtual void run()
     {
       std::cout << "Superhirn integration\n";
@@ -34,34 +52,19 @@ namespace OpenMS
       
       MyMap dummyMap;
       Vec* datavec = new Vec(map_.size(), dummyMap);
-      unsigned int scanIndex = 0;
+      unsigned int scanId = 0;
       
       for (unsigned int s = 0; s < map_.size(); s++)
       {
         const SpectrumType& spectrum = map_[s];
         double rt = spectrum.getRT();
         
-        // determine native id
-        //Int valid_id;
-        Size num_pos=0;
-        String native_id = spectrum.getNativeID();
-        
-        
-        while(!isdigit(native_id[num_pos]) && num_pos<native_id.length())
-        {
-          ++num_pos;
+        bool orderByNativeIds = true; // TODO make this configurable by parameter
+        if (orderByNativeIds)
+          scanId = getNativeScanId(spectrum.getNativeID());
+        else {
+          scanId++;
         }
-        if(num_pos==native_id.length())
-        {
-          std::cout << "Native id could not be determined: " << native_id;
-          return;
-        }
-        else
-        {
-          scanIndex = native_id.substr(num_pos).toInt() - 1;
-//          std::cout << native_id.substr(num_pos).toInt() << ", ";
-        }
-        
         
         vector<double>* vmzvals = new vector<double>();
         vector<double>* vintvals = new vector<double>();
@@ -76,9 +79,8 @@ namespace OpenMS
         
         MyMap m;
         m[rt/60.0] = data;
-        //datavec->push_back(m);
+        unsigned int scanIndex = scanId - 1;
         datavec->at(scanIndex) = m;
-        //scanIndex++;
       }
       
       FeatureFinderAlgorithmSHCtrl ctrl;

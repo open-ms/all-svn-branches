d16219b4e0ef78ff8548efdb1d0bb95ebb975ffa
Bastian Blank <waldi@users.sourceforge.net>
2011-11-17 12:35:14 +0000

    SILACFilter: Document filter methods, make IsotopeDistributionCache a nested class
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/SILACAnalyzer@9184 6adb6e08-d915-0410-941f-83917bcadc18

--- include/OpenMS/FILTERING/DATAREDUCTION/SILACFilter.h
+++ include/OpenMS/FILTERING/DATAREDUCTION/SILACFilter.h
@@ -40,143 +40,6 @@
 
 namespace OpenMS
 {
-  // Prealculate isotope distributions for interesting mass ranges
-  class IsotopeDistributionCache
-  {
-    public:
-      /** @name Accessors
-      */
-      //@{
-      /// returns a pointer to the singleton instance of the isotope distribution cache
-      inline static IsotopeDistributionCache* getInstance()
-      {
-        static IsotopeDistributionCache* cache_ = 0;
-        if (cache_ == 0)
-        {
-          cache_ = new IsotopeDistributionCache;
-        }
-        return cache_;
-      }
-
-      typedef FeatureFinderAlgorithmPickedHelperStructs::TheoreticalIsotopePattern TheoreticalIsotopePattern;
-
-      bool isPrecalculated() const
-      {
-        return (isotope_distributions_.size() != 0);
-      }
-
-      void precalculate(DoubleReal max_mass, DoubleReal mass_window_width, DoubleReal intensity_percentage = 0, DoubleReal intensity_percentage_optional = 0)
-      {
-        isotope_distributions_.clear();
-        max_mass_ = max_mass;
-        mass_window_width_ = mass_window_width;
-        intensity_percentage_optional_ = intensity_percentage_optional;
-        intensity_percentage_ = intensity_percentage;
-
-        Size num_isotopes = std::ceil(max_mass/mass_window_width) + 1;
-
-        //reserve enough space
-        isotope_distributions_.resize(num_isotopes);
-
-        //calculate distribution if necessary
-        for (Size index = 0; index < num_isotopes; ++index)
-        {
-          //log_ << "Calculating iso dist for mass: " << 0.5*mass_window_width_ + index * mass_window_width_ << std::endl;
-          IsotopeDistribution d;
-          d.setMaxIsotope(20);
-          d.estimateFromPeptideWeight(0.5*mass_window_width_ + index * mass_window_width_);
-
-          //trim left and right. And store the number of isotopes on the left, to reconstruct the monoisotopic peak
-          Size size_before = d.size();
-          d.trimLeft(intensity_percentage_optional_);
-          isotope_distributions_[index].trimmed_left = size_before - d.size();
-          d.trimRight(intensity_percentage_optional_);
-
-          for (IsotopeDistribution::Iterator it=d.begin(); it!=d.end(); ++it)
-          {
-            isotope_distributions_[index].intensity.push_back(it->second);
-            //log_ << " - " << it->second << std::endl;
-          }
-
-          //determine the number of optional peaks at the beginning/end
-          Size begin = 0;
-          Size end = 0;
-          bool is_begin = true;
-          bool is_end = false;
-          for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
-          {
-            if (isotope_distributions_[index].intensity[i] < intensity_percentage_)
-            {
-              if (!is_end && !is_begin) is_end = true;
-              if (is_begin) ++begin;
-              else if (is_end) ++end;
-            }
-            else if (is_begin)
-            {
-              is_begin = false;
-            }
-          }
-          isotope_distributions_[index].optional_begin = begin;
-          isotope_distributions_[index].optional_end = end;
-
-          //scale the distibution to a maximum of 1
-          DoubleReal max = 0.0;
-          for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
-          {
-            if (isotope_distributions_[index].intensity[i] > max)
-            {
-              max = isotope_distributions_[index].intensity[i];
-            }
-          }
-
-          isotope_distributions_[index].max = max;
-
-          for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
-          {
-            isotope_distributions_[index].intensity[i] /= max;
-          }
-        }
-      }
-
-      /// Returns the isotope distribution for a certain mass window
-      const TheoreticalIsotopePattern& getIsotopeDistribution(DoubleReal mass) const
-      {
-        //calculate index in the vector
-        Size index = (Size)std::floor(mass / mass_window_width_);
-
-        if (index >= isotope_distributions_.size())
-        {
-          throw Exception::InvalidValue(__FILE__, __LINE__, __PRETTY_FUNCTION__, "IsotopeDistribution not precalculated. Maximum allowed index is " + String(isotope_distributions_.size()), String(index));
-        }
-
-        //Return distribution
-        return isotope_distributions_[index];
-      }
-
-    private:
-      IsotopeDistributionCache()
-      {
-
-      }
-
-      IsotopeDistributionCache(const IsotopeDistributionCache&);
-
-      IsotopeDistributionCache& operator = (const IsotopeDistributionCache&);
-
-      virtual ~IsotopeDistributionCache()
-      {
-
-      }
-
-      /// Vector of precalculated isotope distributions for several mass winows
-      std::vector< TheoreticalIsotopePattern > isotope_distributions_;
-
-      DoubleReal max_mass_;
-      DoubleReal mass_window_width_;
-      DoubleReal intensity_percentage_optional_;
-      DoubleReal intensity_percentage_;
-  };
-
   /**
    * @brief Filter to use for SILACFiltering
    *
@@ -188,10 +51,149 @@ namespace OpenMS
    */   
   class OPENMS_DLLAPI SILACFilter
   {
-    friend class SILACFiltering;
-
     private:
+      friend class SILACFiltering;
+
       typedef FeatureFinderAlgorithmPickedHelperStructs::TheoreticalIsotopePattern TheoreticalIsotopePattern;
+
+      /**
+       * @brief Prealculate isotope distributions for interesting mass ranges
+       */
+      class IsotopeDistributionCache
+      {
+        public:
+          /** @name Accessors
+          */
+          //@{
+          /// returns a pointer to the singleton instance of the isotope distribution cache
+          inline static IsotopeDistributionCache* getInstance()
+          {
+            static IsotopeDistributionCache* cache_ = 0;
+            if (cache_ == 0)
+            {
+              cache_ = new IsotopeDistributionCache;
+            }
+            return cache_;
+          }
+
+          typedef FeatureFinderAlgorithmPickedHelperStructs::TheoreticalIsotopePattern TheoreticalIsotopePattern;
+
+          bool isPrecalculated() const
+          {
+            return (isotope_distributions_.size() != 0);
+          }
+
+          void precalculate(DoubleReal max_mass, DoubleReal mass_window_width, DoubleReal intensity_percentage = 0, DoubleReal intensity_percentage_optional = 0)
+          {
+            isotope_distributions_.clear();
+            max_mass_ = max_mass;
+            mass_window_width_ = mass_window_width;
+            intensity_percentage_optional_ = intensity_percentage_optional;
+            intensity_percentage_ = intensity_percentage;
+
+            Size num_isotopes = std::ceil(max_mass/mass_window_width) + 1;
+
+            //reserve enough space
+            isotope_distributions_.resize(num_isotopes);
+
+            //calculate distribution if necessary
+            for (Size index = 0; index < num_isotopes; ++index)
+            {
+              //log_ << "Calculating iso dist for mass: " << 0.5*mass_window_width_ + index * mass_window_width_ << std::endl;
+              IsotopeDistribution d;
+              d.setMaxIsotope(20);
+              d.estimateFromPeptideWeight(0.5*mass_window_width_ + index * mass_window_width_);
+
+              //trim left and right. And store the number of isotopes on the left, to reconstruct the monoisotopic peak
+              Size size_before = d.size();
+              d.trimLeft(intensity_percentage_optional_);
+              isotope_distributions_[index].trimmed_left = size_before - d.size();
+              d.trimRight(intensity_percentage_optional_);
+
+              for (IsotopeDistribution::Iterator it=d.begin(); it!=d.end(); ++it)
+              {
+                isotope_distributions_[index].intensity.push_back(it->second);
+                //log_ << " - " << it->second << std::endl;
+              }
+
+              //determine the number of optional peaks at the beginning/end
+              Size begin = 0;
+              Size end = 0;
+              bool is_begin = true;
+              bool is_end = false;
+              for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
+              {
+                if (isotope_distributions_[index].intensity[i] < intensity_percentage_)
+                {
+                  if (!is_end && !is_begin) is_end = true;
+                  if (is_begin) ++begin;
+                  else if (is_end) ++end;
+                }
+                else if (is_begin)
+                {
+                  is_begin = false;
+                }
+              }
+              isotope_distributions_[index].optional_begin = begin;
+              isotope_distributions_[index].optional_end = end;
+
+              //scale the distibution to a maximum of 1
+              DoubleReal max = 0.0;
+              for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
+              {
+                if (isotope_distributions_[index].intensity[i] > max)
+                {
+                  max = isotope_distributions_[index].intensity[i];
+                }
+              }
+
+              isotope_distributions_[index].max = max;
+
+              for (Size i = 0; i < isotope_distributions_[index].intensity.size(); ++i)
+              {
+                isotope_distributions_[index].intensity[i] /= max;
+              }
+            }
+          }
+
+          /// Returns the isotope distribution for a certain mass window
+          const TheoreticalIsotopePattern& getIsotopeDistribution(DoubleReal mass) const
+          {
+            //calculate index in the vector
+            Size index = (Size)std::floor(mass / mass_window_width_);
+
+            if (index >= isotope_distributions_.size())
+            {
+              throw Exception::InvalidValue(__FILE__, __LINE__, __PRETTY_FUNCTION__, "IsotopeDistribution not precalculated. Maximum allowed index is " + String(isotope_distributions_.size()), String(index));
+            }
+
+            //Return distribution
+            return isotope_distributions_[index];
+          }
+
+        private:
+          IsotopeDistributionCache()
+          {
+
+          }
+
+          IsotopeDistributionCache(const IsotopeDistributionCache&);
+
+          IsotopeDistributionCache& operator = (const IsotopeDistributionCache&);
+
+          virtual ~IsotopeDistributionCache()
+          {
+
+          }
+
+          /// Vector of precalculated isotope distributions for several mass winows
+          std::vector< TheoreticalIsotopePattern > isotope_distributions_;
+
+          DoubleReal max_mass_;
+          DoubleReal mass_window_width_;
+          DoubleReal intensity_percentage_optional_;
+          DoubleReal intensity_percentage_;
+      };
 
       /**
        * @brief number of peptides [i.e. number of labelled peptides +1, e.g. for SILAC triplet =3]
@@ -264,20 +266,50 @@ namespace OpenMS
       std::vector<std::vector<DoubleReal> > expected_shifts_;
 
       /**
-       * @brief returns true if there exists a SILAC feature at the given position, which corresponds to the filter's properties
+       * @brief Checks if there exists a SILAC feature at the given position in the raw (interpolated) data, which corresponds to the filter's properties
        * @param rt RT value of the position
        * @param mz m/z value of the position
        */
       bool isSILACPattern_(const MSSpectrum<Peak1D> &, const SILACFiltering::SpectrumInterpolation &, DoubleReal mz, DoubleReal picked_mz, const SILACFiltering &, MSSpectrum<Peak1D> &debug, SILACPattern &pattern);
 
-
+      /**
+       * @brief Checks if there exists a SILAC feature at the given position in the picked data
+       */
       bool isSILACPatternPicked_(const MSSpectrum<Peak1D> &, DoubleReal mz, const SILACFiltering &, MSSpectrum<Peak1D> &debug);
+
+      /**
+       * @brief Extracts mass shifts and intensities from the raw (interpolated) data
+       */
       bool extractMzShiftsAndIntensities_(const MSSpectrum<Peak1D> &, const SILACFiltering::SpectrumInterpolation &, DoubleReal mz, DoubleReal picked_mz, const SILACFiltering &);
+
+      /**
+       * @brief Extracts mass shifts and intensities from the picked data
+       */
       bool extractMzShiftsAndIntensitiesPicked_(const MSSpectrum<Peak1D> &, DoubleReal mz, const SILACFiltering &);
+
+      /**
+       * @brief Extracts mass shifts and intensities from the picked data and returns pattern information
+       */
       bool extractMzShiftsAndIntensitiesPickedToPattern_(const MSSpectrum<Peak1D> &, DoubleReal mz, const SILACFiltering &, SILACPattern &pattern);
+
+      /**
+       * @brief Checks all peaks against intensity cutoff
+       */
       bool intensityFilter_();
+
+      /**
+       * @brief Checks peak form correlation between peaks of one isotope
+       */
       bool correlationFilter1_(const SILACFiltering::SpectrumInterpolation &, DoubleReal mz, const SILACFiltering &);
+
+      /**
+       * @brief Checks peak form correlation between peaks of different isotopes
+       */
       bool correlationFilter2_(const SILACFiltering::SpectrumInterpolation &, DoubleReal mz, const SILACFiltering &);
+
+      /**
+       * @brief Checks peak intensities against the averagene model
+       */
       bool averageneFilter_(DoubleReal mz);
 
     public:  

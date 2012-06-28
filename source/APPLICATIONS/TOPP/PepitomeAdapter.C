fdd65ede1cd706608bf1edb7d29cce19b1aab8c1
Matthias Seybold <mseybold@users.sourceforge.net>
2012-06-28 14:31:57 +0000

    beta version of pepitomeadapter finished
    git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/branches/seybold@9830 6adb6e08-d915-0410-941f-83917bcadc18

--- source/APPLICATIONS/TOPP/PepitomeAdapter.C
+++ source/APPLICATIONS/TOPP/PepitomeAdapter.C
@@ -27,7 +27,7 @@
 
 #include <OpenMS/FORMAT/MzMLFile.h>
 #include <OpenMS/FORMAT/IdXMLFile.h>
-#include <OpenMS/FORMAT/MascotInfile.h>
+#include <OpenMS/FORMAT/PepXMLFile.h>
 #include <OpenMS/KERNEL/StandardTypes.h>
 #include <OpenMS/CHEMISTRY/ModificationDefinitionsSet.h>
 #include <OpenMS/CHEMISTRY/ModificationsDB.h>
@@ -155,126 +155,17 @@ protected:
  void registerOptionsAndFlags_()
  {
    registerInputFile_("Pepitome_executable", "<executable>", "", "The 'Pepitome' executable of the Pepitome installation", true, false, StringList::create("skipexists"));
-
-   registerStringOption_("mode", "<type>", "", "Pepitome mode.", true, false);
-   vector<String> mode_strings;
-   mode_strings.push_back("search");
-   mode_strings.push_back("create");
-   setValidStrings_("mode", mode_strings);
-
-   addEmptyLine_();
-
-   registerTOPPSubsection_("create","Create Mode");
-   addText_("General options");
-
-   registerInputFile_("create:in", "<file>", "", "Input file ", false);
-   setValidFormats_("create:in",StringList::create("mzML,mzXML,mzData,mgf,dta,msp,pepXML"));
-   registerOutputFile_("create:outputFileName", "<file>", "", "Output file base name", false);
-   registerInputFile_("create:useProbTable", "<file>", "", "Use probability table in <file>. Only those peptide ions included in the table will be imported. A probability table is a text file with one peptide ion in the format AC[160]DEFGHIK/2 per line. If a probability is supplied following the peptide ion separated by a tab, it will be used to replace the original probability of that library entr", false);
-   registerInputFile_("create:useProteinList", "<file>", "", "Use protein list in <file>. Only those peptide ions associated with proteins in the list will be imported.\n A protein list is a text file with one protein identifier per line. If a number X is supplied following the protein separated by a tab, then at most X peptide ions associated with that protein will be imported.", false);
-   //registerStringOption_("create:remark", "<remark>", "", "Remark. Add a Remark=<remark> comment to all library entries created.", false);
-   registerStringOption_("create:annotatePeaks","<type>", "true", "Annotate peaks.", false, false);
-   registerStringOption_("create:binaryFormat","<type>", "true", "Write library in binary format, which enables quicker search.", false, false);
-   registerStringOption_("create:writeDtaFiles","<type>", "false", "Write all library spectra as .dta files.", false, false);
-   registerStringOption_("create:writeMgfFiles","<type>", "false", "Write all library spectra as one .mgf file.", false, false);
-
-   addEmptyLine_();
-   addText_("PEPXML IMPORT OPTIONS (Applicable with .pepXML file input) ");
-   registerDoubleOption_("create:minimumProbabilityToInclude", "<prob>", 0.0, "Include all spectra identified with probability no less than <prob> in the library.", false);
-   registerStringOption_("create:datasetName", "<name>", "", "Specify a dataset identifier for the file to be imported.", false);
-   registerStringOption_("create:addMzXMLFileToDatasetName", "<type>", "false", "Add the originating mzXML file name to the dataset identifier. Good for keeping track of in which \nMS run the peptide is observed. (Turn off with -co!)", false, false);
-   registerStringOption_("create:setDeamidatedNXST", "<type>", "false", "Set all asparagines (N) in the motif NX(S/T) as deamidated (N[115]). Use for glycocaptured peptides. (Turn off with -cg!).", false, false);
-   registerStringOption_("create:setFragmentation", "<frag>", "","Set the fragmentation type of all spectra, overriding existing information.");
-   registerStringOption_("create:setDeamidatedNXST", "<type>", "false", "Set all asparagines (N) in the motif NX(S/T) as deamidated (N[115]), and all asparagines not in the motif NX(S/T) as unmodified. Use for glycocaptured peptides.", false, false);
-   //registerFlag_("cI", "Set the instrument and acquisition settings of the spectra.");
-
-   addText_("LIBRARY MANIPULATION OPTIONS (Applicable with .splib files)");
-
-   registerStringOption_("create:filterCriteria", "<pred>", "", "<pred> should be in quotes in the form “<attr> <op> <value>”. <attr> can refer to any of the fields and any comment entries. <op> can be ==, !=, <, >, <=, >=, =~ and !~. Multiple predicates can be separated by either & (AND logic) or | (OR logic), but not both. Default is off. ", false);
-   registerStringOption_("create:combineAction", "<test>", "cJU", "Combine action.", false, false);
-   setValidStrings_("create:combineAction", StringList::create("cJU,cJI,CJS,cJH"));
-   registerStringOption_("create:buildAction", "<test>", "", "Build action.", false, false);
-   setValidStrings_("create:buildAction", StringList::create("cAB,cAC,cAQ,cAD,cAM"));
-
-   registerInputFile_("create:refreshDatabase","<file>", "" ,"Refresh protein mappings against the database <file> in FASTA format.", false, false);
-   registerFlag_("create:refreshDeleteUnmapped", "Delete entries whose peptide sequences do not map to any protein during refreshing with -cD option.", true);
-   registerFlag_("create:refreshDeleteMultimapped", "Delete entries whose peptide sequences map to multiple proteins during refreshing with the -cD option.", true);
-
-
-   addEmptyLine_();
-   addText_("CONSENSUS SPECTRUM CREATION OPTIONS (Applicable with -cAC option) ");
-   registerIntOption_("create:minimumNumReplicates", "<num>", 1, "Minimum number of replicates required for each library entry. Peptide ions with fewer than <num> replicates will be excluded from library when creating consensus library.", false);
-   registerStringOption_("create:removeDissimilarReplicates","<type>", "false", "Remove dissimilar replicates before creating consensus spectrum.", false, true);
-   registerDoubleOption_("create:peakQuorum", "<frac>", 0.6, "Specify peak quorum: the fraction of all replicates required to contain a certain peak. Peaks not present in enough replicates will be deleted.", false, true);
-   registerIntOption_("create:maximumNumPeaksUsed", "<num>", 300, "Maximum number of peaks in each replicate to be considered in creating consensus. Only the most intense <num> peaks by intensity will be considered.", false, true);
-   registerIntOption_("create:maximumNumReplicates", "<num>", 100, "Maximum number of replicates used to build consensus spectrum.", false, true);
-   registerIntOption_("create:maximumNumPeaksKept", "<num>", 150, "De-noise single spectra by keeping only the most intense <num> peaks.", false, true);
-   registerStringOption_("create:replicateWeight", "<score>", "c_WGTS", "Select the type of score to weigh and rank the replicates.", false, true);
-   setValidStrings_("create:buildAction", StringList::create("c_WGTS,c_WGTX,c_WGTP"));
-
-   addEmptyLine_();
-   addText_("BEST REPLICATE SELECTION OPTIONS (Applicable with -cAB option) ");
-   //registerIntOption_("create:minimumNumReplicates", "<num>", 1, "Minimum number of replicates required for each library entry. Peptide ions with fewer than <num> replicates will be excluded from library when creating consensus library.", false);
-   registerStringOption_("create:removeDissimilarReplicates","<type>", "false", "Remove dissimilar replicates before creating consensus spectrum.", false, true);
-
-   addEmptyLine_();
-   addText_("QUALITY FILTER OPTIONS (Applicable with -cAQ option) ");
-   registerIntOption_("create:qualityLevelRemove","<level>",0,"Specify the stringency of the quality filter.",false);
-   setMinInt_("create:qualityLevelRemove", 0);
-   setMaxInt_("create:qualityLevelRemove", 5);
-   registerIntOption_("create:qualityLevelMark","<level>",0,"-cL specifies the level for removal, -cl specifies the level for marking.\n<level> = 0: No filter.\n<level> = 1: Remove/mark impure spectra.\n<level> = 2: Also remove/mark spectra with a spectrally similar counterpart in the library that is better.\n<level> = 3: Also remove/mark inquorate entries (defined with -cr) that share no peptide sub-sequences with any other entries in the library.\n<level> = 4: Also remove/mark all singleton entries.\n<level> = 5: Also remove/mark all inquorate entries (defined with -cr).",false);
-   setMinInt_("create:qualityLevelMark", 0);
-   setMaxInt_("create:qualityLevelMark", 5);
-   registerStringOption_("create:qualityPenalizeSingletons", "<type>", "true", "Apply stricter thresholds to singleton spectra during quality filters.", false, false);
-   registerDoubleOption_("create:qualityImmuneProbThreshold", "<tresh>", 0.999, "Specify a probability above which library spectra are immune to quality filters.", false, true);
-                         //0.999, "Specify a probability above which library spectra are immune to quality filters.", true);
-   registerStringOption_("create:qualityImmuneMultipleEngines", "<type>", "true", "Make spectra identified by multiple sequence search engines immune to quality filters.", false, true);
-
-   addEmptyLine_();
-   addText_("DECOY GENERATION OPTIONS (Applicable with -cAD option) ");
-   registerStringOption_("create:decoyConcatenate", "<type>", "false", "Concatenate real and decoy libraries.", false, false);
-   registerIntOption_("create:decoySizeRatio", "<num>", 1, "Specify the (decoy / real) size ratio.", false, true);
-
-   addEmptyLine_();
-   addText_("SEMI-EMPIRICAL SPECTRUM GENERATION OPTIONS (Applicable with -cAM option) ");
-   registerStringOption_("create:allowableModTokens", "<type>", "false", "Specify the set(s) of modifications allowed in semi-empirical spectrum generation by -cAM option.", false, true);
+   registerInputFile_("SpectralLibrary", "<library>", "", "SPTXT formated spectral library", true, false);
+   registerInputFile_("ProteinDatabase", "<fasta>","", "FASTA protein database", true, false);
+   registerInputFile_("mzML", "<file>", "", "MS/MS data filepath in a supported file format", true, false);
+   registerOutputFile_("out", "<file>", "", "idXML Output File", true, false);
 
 
 
 
-   addEmptyLine_();
 
-   registerTOPPSubsection_("search","Search Mode");
-   addText_("(II) Search Mode\nUsage: Pepitome [ options ] <SearchFileName1> [ <SearchFileName2> ... <SearchFileNameN> ]\nwhere: SearchFileNameX = Name(s) of file containing unknown spectra to be searched.\n\t\t(Extension specifies format of file. Supports .mzXML, .mzData, .dta and .msp)");
 
-   registerInputFile_("search:in", "<file>", "", "Input file ");
-   setValidFormats_("search:in",StringList::create("mzML,mzXML,mzData,mgf,dta,msp"));
-   registerInputFile_("search:libraryFile", "<file>", "", "Specify library file. Mandatory unless specified in parameter file. <file> must have .splib extension.", true, false);
-   registerInputFile_("search:databaseFile", "<file>", "", "Specify a sequence database file. This will not affect the search in any way, but this information will be included in the output for any downstream data processing. <file> must have .fasta extension. If not set, Pepitome will try to determine this from the preamble of the library.", true, false);
-   registerStringOption_("search:indexCacheAll", "<type>", "false", "Cache all entries in RAM. Requires a lot of memory (the library will usually be loaded almost in its entirety), but speeds up search for unsorted queries.", false, false);
 
-   addEmptyLine_();
-   addText_("CANDIDATE SELECTION AND SCORING OPTIONS ");
-   registerDoubleOption_("search:indexRetrievalMzTolerance", "<tol>", 3.0, "Specify precursor m/z tolerance in Th. Monoisotopic mass is assumed.", false);
-   registerStringOption_("search:indexRetrievalUseAverage", "<type>", "false", "Use average mass instead of monoisotopic mass.", false, false);
-
-   vector<String> bool_strings;
-   bool_strings.push_back("true");
-   bool_strings.push_back("false");
-   setValidStrings_("create:annotatePeaks", bool_strings);
-   setValidStrings_("create:binaryFormat", bool_strings);
-   setValidStrings_("create:writeDtaFiles", bool_strings);
-   setValidStrings_("create:writeMgfFiles", bool_strings);
-   setValidStrings_("create:addMzXMLFileToDatasetName", bool_strings);
-   setValidStrings_("create:setDeamidatedNXST", bool_strings);
-   setValidStrings_("create:setDeamidatedNXST", bool_strings);
-   setValidStrings_("create:removeDissimilarReplicates", bool_strings);
-   setValidStrings_("create:qualityPenalizeSingletons", bool_strings);
-   setValidStrings_("create:qualityImmuneMultipleEngines", bool_strings);
-   setValidStrings_("create:decoyConcatenate", bool_strings);
-   setValidStrings_("create:allowableModTokens", bool_strings);
-   setValidStrings_("search:indexCacheAll", bool_strings);
-   setValidStrings_("search:indexRetrievalUseAverage", bool_strings);
 
  }
 
@@ -310,7 +201,7 @@ protected:
      tmp.split(" (", version_split);
      String version_line = version_split[0];
 
-     cout << version_line << endl;
+     //cout << version_line << endl;
 
 
      if (getVersion_(version_line, Pepitome_version_i))
@@ -324,21 +215,15 @@ protected:
      }
    }
 
-   QStringList pepXMLFiles;
    QStringList qparam;
 
-   // create parameters
-   if (getStringOption_("mode") == "search")
-   {
-     qparam << "-sF";
-   } else if (getStringOption_("mode") == "create")
-   {
-     qparam << "-cN" << getStringOption_("create:outputFileName").toQString();
-     qparam << "-cP" << QString::number(getDoubleOption_("create:minimumProbabilityToInclude"));
-     qparam << getStringOption_("create:in").toQString();
-   }
+   qparam << "-SpectralLibrary" << getStringOption_("SpectralLibrary").toQString();
+   qparam << "-ProteinDatabase" << getStringOption_("ProteinDatabase").toQString();
+   qparam << getStringOption_("mzML").toQString();
 
-   cout << (String)qparam.join("") << endl;
+   //TODO: parse remaining command line options
+
+   cout << (String)qparam.join("\t") << endl;
 
    Int status = QProcess::execute(Pepitome_executable.toQString(), qparam);
    if (status != 0)
@@ -368,10 +253,17 @@ protected:
    //-------------------------------------------------------------
    // convert pepXML output to idXML
    //-------------------------------------------------------------
+//   String outputfile_name;
+//   outputfile_name = getStringOption_("out");
+   vector<ProteinIdentification> protein_ids;
+   vector<PeptideIdentification> peptide_ids;
+   PepXMLFile pep_xml;
+   IdXMLFile id_xml;
+   //pep_xml.load(String(pepxml_filename.toStdString()), protein_ids, peptide_ids);
+   cout << getStringOption_("out") << endl;
+   id_xml.store(getStringOption_("out"), protein_ids, peptide_ids);
 
 
-   TextFile test_params;
-   test_params.store(getStringOption_("create:outputFileName")); //müll
 
    return EXECUTION_OK;
  }
